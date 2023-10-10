c0fae003bf0efacc556ca637876e7cc9b7f15323
Romaisa alvi <romaisa.uchiha@gmail.com>
2023-10-11 00:29:41 +0500

    final comit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header () {
+    return(
+        <header className="header">
+            <img src="src/assets/meme-logo.png" className="meme--logo"></img>
+            <h3 className="header--title">Meme Generator</h3>
+            <h4 className="header--text">React Course - Project 3</h4>
+        </header>
+    )
+}
